28cef4e2fe2b4d24a2325649d5f6c307880fe3a2
Jules <jlabador@gmail.com>
2019-02-13 18:26:47 -0800

    Initial Commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,9 +12,55 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupViews()
+        
     }
-
-
+    
+    let helloLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.textColor = UIColor.blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Avenir-Heavy", size: 25)
+        
+        return label
+    }()
+    
+    let aLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.textColor = UIColor.blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Avenir-Heavy", size: 25)
+        return label
+    }()
+    
+    let buttonOne: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("hello", for: .normal)
+        button.backgroundColor = UIColor.red
+        
+        return button
+    }()
+    
+    func setupViews() {
+        
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(helloLabel)
+        helloLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 0).isActive = true
+        helloLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: 0).isActive = true
+        
+        view.addSubview(aLabel)
+        aLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 0).isActive = true
+        aLabel.bottomAnchor.constraint(equalTo: helloLabel.topAnchor, constant: -30).isActive = true
+        
+        view.addSubview(buttonOne)
+        buttonOne.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 0).isActive = true
+        buttonOne.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -19).isActive = true
+        buttonOne.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    }
 }
 
